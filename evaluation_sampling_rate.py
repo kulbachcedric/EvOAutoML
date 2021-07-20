efcb060e5a7383a55e8eb11ec69e1501fe82bdbd
Cedric Kulbach <kulbach@fzi.de>
2021-07-20 18:24:04 +0200

    IMRPOVE parallel execution

--- evaluation_sampling_rate.py
+++ evaluation_sampling_rate.py
@@ -1,41 +1,52 @@
+import itertools
+from multiprocessing import Pool
 from pathlib import Path
+from typing import Tuple
 
 from river import ensemble
 from tqdm import tqdm
 
 from EvOAutoML.oaml import EvolutionaryBestClassifier
 from EvOAutoML.config import CLASSIFICATION_TRACKS, AUTOML_PIPELINE, PARAM_GRID, BASE_ESTIMATOR, ENSEMBLE_ESTIMATOR
-
+import pandas as pd
 from EvOAutoML.utils import plot_track
+
+def evaluate_sampling_rate(sampling_rate:int,track_tuple:Tuple):
+    track_name = track_tuple[0]
+    track = track_tuple[1]
+    data = plot_track(
+        track=track,
+        metric_name="Accuracy",
+        models={
+            'EvoAutoML': EvolutionaryBestClassifier(population_size=5, estimator=AUTOML_PIPELINE, param_grid=PARAM_GRID, sampling_rate=sampling_rate),
+            #'Unbounded HTR': (preprocessing.StandardScaler() | tree.HoeffdingTreeClassifier()),
+            ##'SRPC': ensemble.SRPClassifier(model=tree.HoeffdingTreeClassifier(),n_models=10),
+            'Bagging' : ensemble.BaggingClassifier(model=ENSEMBLE_ESTIMATOR),
+            'Ada Boost' : ensemble.AdaBoostClassifier(model=ENSEMBLE_ESTIMATOR),
+            'ARFC' : ensemble.AdaptiveRandomForestClassifier(),
+            'LB' : ensemble.LeveragingBaggingClassifier(model=ENSEMBLE_ESTIMATOR),
+            'Adwin Bagging' : ensemble.ADWINBaggingClassifier(model=ENSEMBLE_ESTIMATOR),
+        },
+        n_samples=10_000,
+        n_checkpoints=1000,
+        result_path=Path(f'./results/evaluation_sampling_rate/{track_name}_{sampling_rate}'),
+        verbose=2
+    )
+    data['sampling_rate'] = len(data)*[sampling_rate]
+    data['track'] = len(data)*[track_name]
+    return data
+
 
 if __name__ == '__main__':
     sampling_rates = [1,5,10,25,50,100,150,200,250,500,750,1000]
-    result_data = None
-    for sampling_rate in  sampling_rates:
-        for track_name, track in tqdm(CLASSIFICATION_TRACKS):
-            data = plot_track(
-                track=track,
-                metric_name="Accuracy",
-                models={
-                    'EvoAutoML': EvolutionaryBestClassifier(population_size=5, estimator=AUTOML_PIPELINE, param_grid=PARAM_GRID, sampling_rate=sampling_rate),
-                    #'Unbounded HTR': (preprocessing.StandardScaler() | tree.HoeffdingTreeClassifier()),
-                    ##'SRPC': ensemble.SRPClassifier(model=tree.HoeffdingTreeClassifier(),n_models=10),
-                    'Bagging' : ensemble.BaggingClassifier(model=ENSEMBLE_ESTIMATOR),
-                    'Ada Boost' : ensemble.AdaBoostClassifier(model=ENSEMBLE_ESTIMATOR),
-                    'ARFC' : ensemble.AdaptiveRandomForestClassifier(),
-                    'LB' : ensemble.LeveragingBaggingClassifier(model=ENSEMBLE_ESTIMATOR),
-                    'Adwin Bagging' : ensemble.ADWINBaggingClassifier(model=ENSEMBLE_ESTIMATOR),
-                },
-                n_samples=3,#_000,
-                n_checkpoints=1,#00,
-                result_path=Path(f'./results/evaluation_sampling_rate/{track_name}_{sampling_rate}'),
-                verbose=2
-            )
-            data['sampling_rate'] = len(data)*[sampling_rate]
-            if result_data is None:
-                result_data = data
-            else:
-                result_data = result_data.append(data)
+
+    testing_configurations = list(itertools.product(sampling_rates,CLASSIFICATION_TRACKS))
+
+    pool = Pool()  # Create a multiprocessing Pool
+    output = pool.starmap(evaluate_sampling_rate, testing_configurations)
+    result_data = pd.concat(output)
+
+
     result_path = Path(f'./results')
     result_path.mkdir(parents=True, exist_ok=True)
     result_path = result_path / 'evaluation_sampling_rate.csv'
