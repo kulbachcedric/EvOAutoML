9a5b8546e7de8abfc06b450b7dde7d6469ded882
Cedric Kulbach <kulbach@fzi.de>
2021-07-26 10:41:13 +0200

    MOVE config

--- EvOAutoML/config.py
+++ EvOAutoML/config.py
@@ -56,13 +56,13 @@ CLASSIFICATION_PARAM_GRID = {
         'HT__max_size' : [10,50],
         'HAT__tie_threshold': [.01, .05, .1],
         'HAT__max_size' : [10,50],
-        'KNN__n_neighbors' : [2,5,10],
-        'KNN__window_size' : [50,100,500],
         'FT__grace_period': [10, 100, 200],
         'FT__max_depth': [10, 20, 50],
         'FT__split_confidence': [1e-7],
         'FT__tie_threshold': [0.05],
         'FT__binary_split': [False],
         'FT__max_size': [50, 100,200],
+        'KNN__n_neighbors': [2, 5, 10],
+        'KNN__window_size': [50, 100, 500],
     })
 }
