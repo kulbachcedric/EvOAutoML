aebe235e78c2430fc09969b23294233f7a7da0b8
Cedric Kulbach <kulbach@fzi.de>
2021-08-03 08:43:24 +0200

    RENAME regression tracks

--- EvOAutoML/tracks/regression_tracks.py
+++ EvOAutoML/tracks/regression_tracks.py
@@ -4,32 +4,32 @@ from river.evaluate import Track
 from river import metrics
 
 
-def trump_track(n_samples=10_000, seed=42):
+def trump_mse_track(n_samples=10_000, seed=42):
     dataset = datasets.TrumpApproval().take(n_samples)
     track = Track("TRUMP Approval + MSE", dataset, metrics.MSE(), n_samples)
     return track
 
-def bikes_track(n_samples=10_000, seed=42):
+def bikes_mse_track(n_samples=10_000, seed=42):
     dataset = datasets.Bikes().take(n_samples)
     track = Track("BIKES + MSE", dataset, metrics.MSE(), n_samples)
     return track
 
-def chickweights_track(n_samples=10_000, seed=42):
+def chickweights_mse_track(n_samples=10_000, seed=42):
     dataset = datasets.ChickWeights().take(n_samples)
     track = Track("ChickWeights + MSE", dataset, metrics.MSE(), n_samples)
     return track
 
-def movielens_track(n_samples=10_000, seed=42):
+def movielens_mse_track(n_samples=10_000, seed=42):
     dataset = datasets.MovieLens100K().take(n_samples)
     track = Track("MovieLens100K + MSE", dataset, metrics.MSE(), n_samples)
     return track
 
-def restaurants_track(n_samples=10_000, seed=42):
+def restaurants_mse_track(n_samples=10_000, seed=42):
     dataset = datasets.Restaurants().take(n_samples)
     track = Track("RESTAURANTS + MSE", dataset, metrics.MSE(), n_samples)
     return track
 
-def taxis_track(n_samples=10_000, seed=42):
+def taxis_mse_track(n_samples=10_000, seed=42):
     dataset = datasets.Taxis().take(n_samples)
     track = Track("TAXIS + MSE", dataset, metrics.MSE(), n_samples)
     return track
