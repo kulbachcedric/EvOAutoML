ad786a5f7121bd1f10f0674734d3912d4d22bc29
Cedric Kulbach <kulbach@fzi.de>
2021-08-03 09:50:44 +0200

    First working regression

--- EvOAutoML/config.py
+++ EvOAutoML/config.py
@@ -1,13 +1,19 @@
 from river import compose, preprocessing, dummy, feature_extraction, tree, linear_model, neural_net, naive_bayes, \
     time_series, neighbors, optim
-from river.neighbors import KNNClassifier
+from river.neighbors import KNNClassifier, KNNRegressor
 
-from EvOAutoML.pipelinehelper import PipelineHelperTransformer, PipelineHelperClassifier
+from EvOAutoML.pipelinehelper import PipelineHelperTransformer, PipelineHelperClassifier, PipelineHelperRegressor
 from EvOAutoML.tracks.classification_tracks import random_rbf_track, agrawal_track, anomaly_sine_track, \
     concept_drift_track, \
     hyperplane_track, mixed_track, sea_track, sine_track, stagger_track, elec2_track, bananas_track, creditcard_track, \
     higgs_track, imagesegments_track, insects_track, maliciousURL_track, music_track, pishing_track, \
     smsspam_track, trec07_track
+from EvOAutoML.tracks.regression_tracks import *
+
+POPULATION_SIZE = 10
+
+ENSEMBLE_CLASSIFIER = tree.HoeffdingTreeClassifier
+ENSEMBLE_REGRESSOR = tree.HoeffdingTreeRegressor
 
 CLASSIFICATION_TRACKS = [
     ('Random RBF', random_rbf_track),
@@ -31,10 +37,14 @@ CLASSIFICATION_TRACKS = [
     ('SMS Spam', smsspam_track),
     ('TREC', trec07_track)
 ]
-
-POPULATION_SIZE = 10
-
-ENSEMBLE_CLASSIFIER = tree.HoeffdingTreeClassifier
+REGRESSION_TRACKS = [
+    ('Trump Approval', trump_track),
+    ('Bikes', bikes_track),
+    ('Chick Weights', chickweights_track),
+    ('Movielens', movielens_track),
+    ('Restaurants', restaurants_track),
+    ('Taxi', taxis_track)
+]
 
 AUTOML_CLASSIFICATION_PIPELINE = compose.Pipeline(
     ('Scaler', PipelineHelperTransformer([
@@ -63,7 +73,6 @@ AUTOML_CLASSIFICATION_PIPELINE = compose.Pipeline(
     ]))
 )
 
-
 CLASSIFICATION_PARAM_GRID = {
     #'Scaler': automl_pipeline.steps['Scaler'].generate({}),
     #'FeatureExtractor' : AUTOML_PIPELINE.steps['FeatureExtractor'].generate({
@@ -78,12 +87,68 @@ CLASSIFICATION_PARAM_GRID = {
         'HT__max_depth' : [10,30,60,10,30,60],
         'HT__grace_period': [10, 100, 200,10, 100, 200],
         'HT__max_size': [5,10],
+        'LR__loss': [optim.losses.BinaryLoss,optim.losses.CrossEntropy],
+        'LR__l2': [.0,.01,.001],
+        'LR__optimizer': [optim.SGD,optim.Adam],
+        'KNN__n_neighbors': [1,5,20],
+        'KNN__window_size': [100,500,1000],
+        'KNN__weighted': [True, False],
+        'KNN__p': [1,2]
+
+        #'HAT__tie_threshold': [.01, .05, .1],
+        #'HAT__max_size' : [10,50],
+
+        #'FT__max_depth': [10, 20, 50],
+        #'FT__split_confidence': [1e-7],
+        #'FT__tie_threshold': [0.05],
+        #'FT__binary_split': [False],
+        #'FT__max_size': [50, 100,200],
+    })
+}
+
+AUTOML_REGRESSION_PIPELINE = compose.Pipeline(
+    ('Scaler', PipelineHelperTransformer([
+        ('StandardScaler', preprocessing.StandardScaler()),
+        ('MinMaxScaler', preprocessing.MinMaxScaler()),
+        ('MinAbsScaler', preprocessing.MaxAbsScaler()),
+        # todo create dummy
+        #('RobustScaler', preprocessing.RobustScaler()),
+        #('AdaptiveStandardScaler', preprocessing.AdaptiveStandardScaler()),
+        #('LDA', preprocessing.LDA()),
+
+    ])),
+    ('Regressor', PipelineHelperRegressor([
+        ('HT', tree.HoeffdingTreeRegressor()),
+        #('FT', tree.ExtremelyFastDecisionTreeClassifier()),
+        #('LR', linear_model.LinearRegression()),
+        #('HAT', tree.HoeffdingAdaptiveTreeClassifier()),
+        #('GNB', naive_bayes.GaussianNB()),
+        #('MNB', naive_bayes.MultinomialNB()),
+        #('PAC', linear_model.PAClassifier()),
+        ('KNN', neighbors.KNNRegressor()),
+    ]))
+)
+
+REGRESSION_PARAM_GRID = {
+    #'Scaler': automl_pipeline.steps['Scaler'].generate({}),
+    #'FeatureExtractor' : AUTOML_PIPELINE.steps['FeatureExtractor'].generate({
+    #    'PolynomialExtender__degree' : [1,2],
+    #    'PolynomialExtender__include_bias' : [True,False],
+        #'RBF__n_components' : [2,10]
+    #}),
+    'Regressor' : AUTOML_REGRESSION_PIPELINE.steps['Regressor'].generate({
+        #'HT__tie_threshold': [.01, .05, .1],
+        #'HT__max_size' : [10,50],
+        'HT__binary_split' : [True, False],
+        'HT__max_depth' : [10,30,60,10,30,60],
+        'HT__grace_period': [10, 100, 200,10, 100, 200],
+        'HT__max_size': [5,10],
         #'LR__loss': [optim.losses.BinaryLoss,optim.losses.CrossEntropy],
         #'LR__l2': [.0,.01,.001],
         #'LR__optimizer': [optim.SGD,optim.Adam],
         'KNN__n_neighbors': [1,5,20],
         'KNN__window_size': [100,500,1000],
-        'KNN__weighted': [True, False],
+        #'KNN__weighted': [True, False],
         'KNN__p': [1,2]
 
         #'HAT__tie_threshold': [.01, .05, .1],
---
+++ evaluation_regression_ensemble.py
@@ -0,0 +1,46 @@
+from multiprocessing import Pool
+from pathlib import Path
+
+from river import tree, ensemble
+from river.linear_model import LinearRegression
+from river.neighbors import KNNClassifier
+from tqdm import tqdm
+
+from EvOAutoML.classification import EvolutionaryBestClassifier
+from EvOAutoML.config import CLASSIFICATION_TRACKS, AUTOML_CLASSIFICATION_PIPELINE, CLASSIFICATION_PARAM_GRID, \
+    ENSEMBLE_CLASSIFIER, POPULATION_SIZE, AUTOML_REGRESSION_PIPELINE, REGRESSION_PARAM_GRID, ENSEMBLE_REGRESSOR, \
+    REGRESSION_TRACKS
+from EvOAutoML.regression import EvolutionaryBestRegressor
+
+from EvOAutoML.utils import plot_track
+
+
+
+
+
+def evaluate_ensemble(track_tuple):
+    track = track_tuple[1]
+    plot_track(
+        track=track,
+        metric_name="MSE",
+        models={
+            'EvoAutoML': EvolutionaryBestRegressor(population_size=POPULATION_SIZE, estimator=AUTOML_REGRESSION_PIPELINE,
+                                                    param_grid=REGRESSION_PARAM_GRID, sampling_rate=250),
+            # 'Unbounded HTR': (preprocessing.StandardScaler() | tree.HoeffdingTreeClassifier()),
+            ##'SRPC': ensemble.SRPClassifier(model=tree.HoeffdingTreeClassifier(),n_models=10),
+
+            'ARF': ensemble.AdaptiveRandomForestRegressor(),
+            'Leveraging Bagging': ensemble.BaggingRegressor(model=ENSEMBLE_REGRESSOR()),
+        },
+        n_samples=10_000,
+        n_checkpoints=1000,
+        result_path=Path(f'./results/regression/evaluation_ensemble'),
+        verbose=2
+    )
+
+
+if __name__ == '__main__':
+
+    evaluate_ensemble(REGRESSION_TRACKS[0])
+    #pool = Pool(60)  # Create a multiprocessing Pool
+    #output = pool.map(evaluate_ensemble, REGRESSION_TRACKS)
