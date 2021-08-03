13f6a760dd1d2a08355adf493868b1bcd22e8655
Cedric Kulbach <kulbach@fzi.de>
2021-08-03 09:58:22 +0200

    REFACTOR Regrssion

--- evaluation_regression_single.py
+++ evaluation_regression_single.py
@@ -6,7 +6,8 @@ from tqdm import tqdm
 
 from EvOAutoML.classification import EvolutionaryBestClassifier
 from EvOAutoML.config import CLASSIFICATION_TRACKS, AUTOML_CLASSIFICATION_PIPELINE, CLASSIFICATION_PARAM_GRID, \
-    POPULATION_SIZE
+    POPULATION_SIZE, REGRESSION_TRACKS
+from EvOAutoML.regression import EvolutionaryBestRegressor
 
 from EvOAutoML.utils import plot_track
 
@@ -14,27 +15,27 @@ def evaluate_single(track_dict):
     track = track_dict[1]
     plot_track(
         track=track,
-        metric_name="Accuracy",
+        metric_name="MSE",
         models={
-            'EvoAutoML': EvolutionaryBestClassifier(population_size=POPULATION_SIZE, estimator=AUTOML_CLASSIFICATION_PIPELINE,
+            'EvoAutoML': EvolutionaryBestRegressor(population_size=POPULATION_SIZE, estimator=AUTOML_CLASSIFICATION_PIPELINE,
                                                     param_grid=CLASSIFICATION_PARAM_GRID, sampling_rate=250),
             # 'Unbounded HTR': (preprocessing.StandardScaler() | tree.HoeffdingTreeClassifier()),
             ##'SRPC': ensemble.SRPClassifier(model=tree.HoeffdingTreeClassifier(),n_models=10),
-            'Hoeffding Tree': tree.HoeffdingTreeClassifier(),
+            'Hoeffding Tree': tree.HoeffdingTreeRegressor(),
             # 'FT',:tree.ExtremelyFastDecisionTreeClassifier(),
-            'Linear Regression': linear_model.LogisticRegression(),
+            'Linear Regression': linear_model.LinearRegression(),
             # ('HAT', tree.HoeffdingAdaptiveTreeClassifier()),
-            'GaussianNB': naive_bayes.GaussianNB(),
+            #'GaussianNB': naive_bayes.GaussianNB(),
             # ('MNB', naive_bayes.MultinomialNB()),
             # ('PAC', linear_model.PAClassifier()),
-            'KNN': neighbors.KNNClassifier(),
+            'KNN': neighbors.KNNRegressor(),
         },
         n_samples=10_000,
         n_checkpoints=1000,
-        result_path=Path(f'./results/classification/evaluation_single'),
+        result_path=Path(f'./results/regression/evaluation_single'),
         verbose=2)
 
 if __name__ == '__main__':
     #evaluate_single(CLASSIFICATION_TRACKS[0])
     pool = Pool(60)  # Create a multiprocessing Pool
-    output = pool.map(evaluate_single, CLASSIFICATION_TRACKS)
+    output = pool.map(evaluate_single, REGRESSION_TRACKS)
