0500c755d438ac3cda0e46393605842f9c5948f5
Cedric Kulbach <kulbach@fzi.de>
2021-08-05 12:15:58 +0200

    Add Leveraging Bagging Classifier

--- EvOAutoML/classification.py
+++ EvOAutoML/classification.py
@@ -3,7 +3,7 @@ import typing
 
 from river import base
 
-from EvOAutoML.base.estimator import EvolutionaryBaggingEstimator
+from EvOAutoML.base.estimator import EvolutionaryBaggingEstimator, EvolutionaryLeveragingBaggingEstimator
 
 
 class EvolutionaryBaggingClassifier(EvolutionaryBaggingEstimator, base.Classifier):
@@ -20,4 +20,15 @@ class EvolutionaryBaggingClassifier(EvolutionaryBaggingEstimator, base.Classifie
             return {label: proba / total for label, proba in y_pred.items()}
         return y_pred
 
+class EvolutionaryLeveragingBaggingClassifer(EvolutionaryLeveragingBaggingEstimator):
+    def predict_proba_one(self, x):
+        """Averages the predictions of each classifier."""
 
+        y_pred = collections.Counter()
+        for classifier in self:
+            y_pred.update(classifier.predict_proba_one(x))
+
+        total = sum(y_pred.values())
+        if total > 0:
+            return {label: proba / total for label, proba in y_pred.items()}
+        return y_pred
