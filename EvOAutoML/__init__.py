f9d77e10d6b907f22277f861fe03109fba979c2c
Cedric Kulbach <cedric.kulbahc@googlemail.com>
2022-04-29 10:35:40 +0200

    Some changes

---
+++ EvOAutoML/__init__.py
@@ -0,0 +1,14 @@
+from . import (
+    classification,
+    regression,
+    utils,
+    base
+)
+from .__version__ import __version__  # noqa: F401
+
+__all__ = [
+    "classification",
+    "regression",
+    "utils",
+    "base"
+]
---
+++ EvOAutoML/base/__init__.py
@@ -0,0 +1,8 @@
+from . import (
+    estimator
+)
+from .__version__ import __version__  # noqa: F401
+
+__all__ = [
+    "estimator",
+]
