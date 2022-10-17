020ce87025fd639032c12e680615ebd12fe8d14c
Cedric Kulbach <cedric.kulbahc@googlemail.com>
2022-10-17 16:37:20 +0200

    ADD documentation for bagging.py

--- EvOAutoML/classification/bagging.py
+++ EvOAutoML/classification/bagging.py
@@ -108,7 +108,75 @@ class EvolutionaryBaggingClassifier(EvolutionaryBaggingEstimator, base.Classifie
         return y_pred
 
 class EvolutionaryOldestBaggingClassifier(EvolutionaryBaggingOldestEstimator, base.Classifier):
+    """
+    Evolutionary Oldest Bagging Classifier follows the Oza Bagging approach to update
+    the population of estimator pipelines. It mutates the population by removing the
+    oldest model configuration.
 
+    Parameters
+    ----------
+    model
+        A river model or model pipeline that can be configured
+        by the parameter grid.
+    param_grid
+        A parameter grid, that represents the configuration space of the model.
+    population_size
+        The population size estimates the size of the population as
+        well as the size of the ensemble used for the prediction.
+    sampling_size
+        The sampling size estimates how many models are mutated
+        within one mutation step.
+    metric
+        The river metric that should be optimised.
+    sampling_rate
+        The sampling rate estimates the number of samples that are executed
+        before a mutation step takes place.
+    seed
+        Random number generator seed for reproducibility.
+
+    Examples
+    --------
+    >>> from river import datasets
+    >>> from river import ensemble
+    >>> from river import evaluate
+    >>> from river import linear_model
+    >>> from river import metrics
+    >>> from river import optim
+    >>> from river import preprocessing
+    >>> dataset = datasets.Phishing()
+    >>> model = ensemble.EvolutionaryBaggingOldestClassifer(
+    ...     model=compose.Pipeline(
+    ...         ('Scaler', PipelineHelperTransformer([
+    ...             ('StandardScaler', preprocessing.StandardScaler()),
+    ...             ('MinMaxScaler', preprocessing.MinMaxScaler()),
+    ...             ('MinAbsScaler', preprocessing.MaxAbsScaler()),
+    ...         ])),
+    ...         ('Classifier', PipelineHelperClassifier([
+    ...             ('HT', tree.HoeffdingTreeClassifier()),
+    ...             ('LR', linear_model.LogisticRegression()),
+    ...             ('GNB', naive_bayes.GaussianNB()),
+    ...             ('KNN', neighbors.KNNClassifier()),
+    ...         ]))
+    ...     ),
+    ...     param_grid={
+    ...         'Scaler': AUTOML_CLASSIFICATION_PIPELINE.steps['Scaler'].generate({}),
+    ...         'Classifier': AUTOML_CLASSIFICATION_PIPELINE.steps['Classifier'].generate({
+    ...             'HT__max_depth': [10, 30, 60, 10, 30, 60],
+    ...             'HT__grace_period': [10, 100, 200, 10, 100, 200],
+    ...             'HT__max_size': [5, 10],
+    ...             'LR__l2': [.0,.01,.001],
+    ...             'KNN__n_neighbors': [1, 5, 20],
+    ...             'KNN__window_size': [100, 500, 1000],
+    ...             'KNN__weighted': [True, False],
+    ...             'KNN__p': [1, 2],
+    ...         })
+    ...     },
+    ...     seed=42
+    ... )
+    >>> metric = metrics.F1()
+    >>> evaluate.progressive_val_score(dataset, model, metric)
+    F1: 88.73%
+    """
 
     def __init__(self,
                  model=AUTOML_CLASSIFICATION_PIPELINE,
