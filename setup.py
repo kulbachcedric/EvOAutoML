18bd7ad0d497e7bba8be476399b575d9727d2818
Cedric Kulbach <cedric.kulbahc@googlemail.com>
2022-10-19 00:25:22 +0200

    update numpy

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ REQUIRES_PYTHON = ">=3.6.0"
 
 # Package requirements.
 base_packages = [
-    "numpy~=1.22.0",
+    "numpy~=1.23.4",
     "scikit-learn==1.0.2",
     "scikit-surprise==1.1.1",
     "pandas~=1.3.2",
